d97778bbbc6069c7336448a0b65db3188703458b
RidvanYesiloglu <ridvan@stanford.edu>
2022-06-28 16:50:22 -0700

    d

--- data_analysis/merge_pt_vols.py
+++ data_analysis/merge_pt_vols.py
@@ -34,20 +34,20 @@ def main(pt_id=None):
             all_vols[vol_no] = np.reshape(img.transpose(), vol_size, order="F")
         finish_time = time.perf_counter()
         #all_vols = np.load(all_data_folder+'/'+pt_id+'/all_vols.npy')
-        np.save(all_data_folder+'/'+pt_id+'/all_vols',all_vols)
+        #np.save(all_data_folder+'/'+pt_id+'/all_vols',all_vols)
         print(f"Elapsed total for the patient: {finish_time-start_time} seconds")
         print('3D PSNRs wrt the initial image are being plotted.')
         make_3dpsnr_wrti_pl_from_4d_data.main(all_vols, pt_id)
-        #print('The gifs for the data is being plotted and saved.')
-        # processes = []
-        # for plot_most_fluc in [False, True]:
-        #     for ax_cr_sg in [0,1,2]:
-        #         p = multiprocessing.Process(target = make_gif_from_4d_data.main, args=(all_vols, pt_id, ax_cr_sg, plot_most_fluc,))
-        #         p.start()
-        #         processes.append(p)
-        # # Join all the processes 
-        # for p in processes:
-        #     p.join()
+        print('The gifs for the data is being plotted and saved.')
+        processes = []
+        for plot_most_fluc in [False, True]:
+            for ax_cr_sg in [0,1,2]:
+                p = multiprocessing.Process(target = make_gif_from_4d_data.main, args=(all_vols, pt_id, ax_cr_sg, plot_most_fluc,))
+                p.start()
+                processes.append(p)
+        # Join all the processes 
+        for p in processes:
+            p.join()
         finish_time = time.perf_counter()
         print(f"Elapsed total for the patient: {finish_time-start_time} seconds, done.")
 if __name__ == "__main__":

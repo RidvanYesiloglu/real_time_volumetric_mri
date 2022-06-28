40e0ed94138d09972e416cde8c9987776388230d
RidvanYesiloglu <ridvan@stanford.edu>
2022-06-27 20:54:30 -0700

    Update make_gif_from_4d_data.py

--- make_gif_from_4d_data.py
+++ make_gif_from_4d_data.py
@@ -35,85 +35,96 @@ def main(all_vols, pt_id, ax_cr_sg, plot_most_fluc=False):
     if not os.path.exists(ind_ims_dir):
         os.makedirs(ind_ims_dir)
         
-    psnrs = calc_psnrs(all_vols, ax_cr_sg)  
-    cmap = mpl.cm.get_cmap('jet_r')
-    (min_psnr, max_psnr) = (psnrs.min(), min(psnrs.max(),100))
-    norm = mpl.colors.Normalize(vmin=min_psnr, vmax=max_psnr)
-    nrows = 2 if plot_most_fluc else 4
-    ncols = int(2*nrows) if ax_cr_sg == 0 else int(2.5*nrows)
-    figsize = (16,7.5) if (ax_cr_sg == 0 and plot_most_fluc) else (16,7.5) if (ax_cr_sg == 0 and (not plot_most_fluc))\
-        else (16,9.5) if (ax_cr_sg != 0 and plot_most_fluc) else (19.5,11)
-     #13.8 8.5
-    filenames = []
-    for t in np.arange(0, all_vols.shape[0]):
-        filename = f'{ind_ims_dir}/frame_{t}.png'
-        filenames.append(filename)
+    psnrs = calc_psnrs(all_vols, ax_cr_sg)
+    
+    
+    
+    minind = psnrs.min(0).argmin()
+    plt.figure()
+    plt.plot(psnrs[:,minind])
+    plt.show()
+    plt.savefig('minpsnrli_notsorted', dpi=96, bbox_inches='tight')
+    plt.close()
+    
+    
+    # cmap = mpl.cm.get_cmap('jet_r')
+    # (min_psnr, max_psnr) = (psnrs.min(), min(psnrs.max(),100))
+    # norm = mpl.colors.Normalize(vmin=min_psnr, vmax=max_psnr)
+    # nrows = 2 if plot_most_fluc else 4
+    # ncols = int(2*nrows) if ax_cr_sg == 0 else int(2.5*nrows)
+    # figsize = (16,7.5) if (ax_cr_sg == 0 and plot_most_fluc) else (16,7.5) if (ax_cr_sg == 0 and (not plot_most_fluc))\
+    #     else (16,9.5) if (ax_cr_sg != 0 and plot_most_fluc) else (19.5,11)
+    #  #13.8 8.5
+    # filenames = []
+    # for t in np.arange(0, all_vols.shape[0]):
+    #     filename = f'{ind_ims_dir}/frame_{t}.png'
+    #     filenames.append(filename)
         
-        # last frame of each viz stays longer
-        if (t == (all_vols.shape[0]-1)):
-            for t in range(5):
-                filenames.append(filename)
+    #     # last frame of each viz stays longer
+    #     if (t == (all_vols.shape[0]-1)):
+    #         for t in range(5):
+    #             filenames.append(filename)
             
-        # save img
-        sl_nos = psnrs.min(0).argsort()[:nrows*ncols] if plot_most_fluc else np.arange(0,(nrows*ncols-1)*(psnrs.shape[1]//(nrows*ncols-1))+1,psnrs.shape[1]//(nrows*ncols-1))
-        fig,ax = plt.subplots(nrows,ncols, figsize=figsize)
-        for i in range(nrows):
-            for j in range(ncols):
-                sl_no = sl_nos[ncols*i+j]
-                if ax_cr_sg == 0:
-                    im_to_show = all_vols[t,...,sl_no]
-                elif ax_cr_sg == 1:
-                    im_to_show = all_vols[t,:,sl_no,:]
-                elif ax_cr_sg == 2:
-                    im_to_show = all_vols[t,sl_no,:,:]
-                im = ax[i,j].imshow(im_to_show,cmap='gray', interpolation='none')
-                ax[i,j].axis('off')
-                ps = psnrs[t,sl_no]
-                ps_color = cmap((ps-min_psnr)/(max_psnr-min_psnr))
-                # Create a Rectangle patch
-                rect = patches.Rectangle((0, 0), im_to_show.shape[1], im_to_show.shape[0], linewidth=5, edgecolor=ps_color, facecolor='none')
-                # Add the patch to the Axes
-                ax[i,j].add_patch(rect)
-                #print(f'max is {min_psnr}, min is {max_psnr}')
-                ax[i,j].set_title('Slice {}'.format(sl_no))
-                ax[i,j].text(0.5,-0.1+0.05*plot_most_fluc-0.01*(ax_cr_sg==0)+0.03*((ax_cr_sg!=0)and(not plot_most_fluc)), '({:.1f} dB)'.format(ps), color=ps_color, size=10, ha="center", transform=ax[i,j].transAxes)
-                divider = make_axes_locatable(ax[i,j])
-                cax = divider.append_axes('right', size='5%', pad=0.05)
-                fig.colorbar(im, cax=cax, orientation='vertical')
-        plt.subplots_adjust(left=0.01, right=0.90, bottom=0.05, top=0.935, wspace=0.32)
-        cbar_ax = fig.add_axes([0.94, 0.15, 0.02, 0.7])
-        cb1 = mpl.colorbar.ColorbarBase(cbar_ax, cmap=cmap, norm=norm, orientation='vertical')
-        cb1.set_label('PSNR wrt the Initial Image (dB)')
-        plt.show()
-        if plot_most_fluc:
-            if ax_cr_sg == 0:
-                plt.suptitle(f"Most Fluctuating Axial Images ({pt_id}, Time Point: {t:3d})")
-            elif ax_cr_sg == 1:
-                plt.suptitle(f"Most Fluctuating Coronal Images ({pt_id}, Time Point: {t:3d})")
-            elif ax_cr_sg == 2:
-                plt.suptitle(f"Most Fluctuating Sagittal Images ({pt_id}, Time Point: {t:3d})")
-        else:
-            if ax_cr_sg == 0:
-                plt.suptitle(f"Axial Images ({pt_id}, Time Point: {t:3d})")
-            elif ax_cr_sg == 1:
-                plt.suptitle(f"Coronal Images ({pt_id}, Time Point: {t:3d})")
-            elif ax_cr_sg == 2:
-                plt.suptitle(f"Sagittal Images ({pt_id}, Time Point: {t:3d})")        
-        plt.savefig(filename, dpi=96, bbox_inches='tight')
-        plt.close()
-    print('Charts saved\n')
-    # Build GIF
-    print('Creating gif\n')
-    with imageio.get_writer(f'/raid/yesiloglu/data/real_time_volumetric_mri/{pt_id}/temporal_evol_gifs/{gif_name}.gif', mode='I') as writer:
-        for filename in filenames:
-            image = imageio.imread(filename)
-            writer.append_data(image)
-    print('Gif saved\n')
-    # print('Removing Images\n')
-    # Remove files
-    # for filename in set(filenames):
-    #     os.remove(filename)
-    print('DONE')
+    #     # save img
+    #     sl_nos = psnrs.min(0).argsort()[:nrows*ncols] if plot_most_fluc else np.arange(0,(nrows*ncols-1)*(psnrs.shape[1]//(nrows*ncols-1))+1,psnrs.shape[1]//(nrows*ncols-1))
+    #     fig,ax = plt.subplots(nrows,ncols, figsize=figsize)
+    #     for i in range(nrows):
+    #         for j in range(ncols):
+    #             sl_no = sl_nos[ncols*i+j]
+    #             if ax_cr_sg == 0:
+    #                 im_to_show = all_vols[t,...,sl_no]
+    #             elif ax_cr_sg == 1:
+    #                 im_to_show = all_vols[t,:,sl_no,:]
+    #             elif ax_cr_sg == 2:
+    #                 im_to_show = all_vols[t,sl_no,:,:]
+    #             im = ax[i,j].imshow(im_to_show,cmap='gray', interpolation='none')
+    #             ax[i,j].axis('off')
+    #             ps = psnrs[t,sl_no]
+    #             ps_color = cmap((ps-min_psnr)/(max_psnr-min_psnr))
+    #             # Create a Rectangle patch
+    #             rect = patches.Rectangle((0, 0), im_to_show.shape[1], im_to_show.shape[0], linewidth=5, edgecolor=ps_color, facecolor='none')
+    #             # Add the patch to the Axes
+    #             ax[i,j].add_patch(rect)
+    #             #print(f'max is {min_psnr}, min is {max_psnr}')
+    #             ax[i,j].set_title('Slice {}'.format(sl_no))
+    #             ax[i,j].text(0.5,-0.1+0.05*plot_most_fluc-0.01*(ax_cr_sg==0)+0.03*((ax_cr_sg!=0)and(not plot_most_fluc)), '({:.1f} dB)'.format(ps), color=ps_color, size=10, ha="center", transform=ax[i,j].transAxes)
+    #             divider = make_axes_locatable(ax[i,j])
+    #             cax = divider.append_axes('right', size='5%', pad=0.05)
+    #             fig.colorbar(im, cax=cax, orientation='vertical')
+    #     plt.subplots_adjust(left=0.01, right=0.90, bottom=0.05, top=0.935, wspace=0.32)
+    #     cbar_ax = fig.add_axes([0.94, 0.15, 0.02, 0.7])
+    #     cb1 = mpl.colorbar.ColorbarBase(cbar_ax, cmap=cmap, norm=norm, orientation='vertical')
+    #     cb1.set_label('PSNR wrt the Initial Image (dB)')
+    #     plt.show()
+    #     if plot_most_fluc:
+    #         if ax_cr_sg == 0:
+    #             plt.suptitle(f"Most Fluctuating Axial Images ({pt_id}, Time Point: {t:3d})")
+    #         elif ax_cr_sg == 1:
+    #             plt.suptitle(f"Most Fluctuating Coronal Images ({pt_id}, Time Point: {t:3d})")
+    #         elif ax_cr_sg == 2:
+    #             plt.suptitle(f"Most Fluctuating Sagittal Images ({pt_id}, Time Point: {t:3d})")
+    #     else:
+    #         if ax_cr_sg == 0:
+    #             plt.suptitle(f"Axial Images ({pt_id}, Time Point: {t:3d})")
+    #         elif ax_cr_sg == 1:
+    #             plt.suptitle(f"Coronal Images ({pt_id}, Time Point: {t:3d})")
+    #         elif ax_cr_sg == 2:
+    #             plt.suptitle(f"Sagittal Images ({pt_id}, Time Point: {t:3d})")        
+    #     plt.savefig(filename, dpi=96, bbox_inches='tight')
+    #     plt.close()
+    # print('Charts saved\n')
+    # # Build GIF
+    # print('Creating gif\n')
+    # with imageio.get_writer(f'/raid/yesiloglu/data/real_time_volumetric_mri/{pt_id}/temporal_evol_gifs/{gif_name}.gif', mode='I') as writer:
+    #     for filename in filenames:
+    #         image = imageio.imread(filename)
+    #         writer.append_data(image)
+    # print('Gif saved\n')
+    # # print('Removing Images\n')
+    # # Remove files
+    # # for filename in set(filenames):
+    # #     os.remove(filename)
+    # print('DONE')
     
     
 if __name__ == "__main__":

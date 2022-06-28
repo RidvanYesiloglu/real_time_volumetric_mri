ad99d58a37a04d3e407ccce91b768a93dae965bc
RidvanYesiloglu <ridvan@stanford.edu>
2022-06-28 16:41:06 -0700

    Update make_3dpsnr_wrti_pl_from_4d_data.py

--- make_3dpsnr_wrti_pl_from_4d_data.py
+++ make_3dpsnr_wrti_pl_from_4d_data.py
@@ -4,7 +4,7 @@ import numpy as np
 def calc_3dpsnrs_wrti(all_vols):
     return 20*np.log((all_vols[0:1]**2).max((1,2,3)))-10*np.log(((all_vols[0:1]-all_vols)**2).mean((1,2,3)))
 
-def main(all_vols):
+def main(all_vols, pt_id):
     # Calculate 3d PSNRs with respect to the initial image
     psnrs = calc_3dpsnrs_wrti(all_vols)
     # Create and save the plot of PSNRs:
@@ -14,7 +14,7 @@ def main(all_vols):
     ax.set_xlabel('Time Index')
     ax.set_ylabel('PSNR (dB)')
     plt.show()
-    plt.savefig('/raid/yesiloglu/data/real_time_volumetric_mri/temporal_evol_gifs/3dpsnrs_wrt_init', dpi=96, bbox_inches='tight')
+    plt.savefig('/raid/yesiloglu/data/real_time_volumetric_mri/{pt_id}/temporal_evol_gifs/3dpsnrs_wrt_init', dpi=96, bbox_inches='tight')
     plt.close()
     
 if __name__ == "__main__":

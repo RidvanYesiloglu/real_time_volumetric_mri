fcc67cf4ffeb4c74b1c51102783e4e1ff93264af
RidvanYesiloglu <ridvan@stanford.edu>
2022-06-28 18:39:50 -0700

    g

--- data_analysis/make_3dpsnr_wrti_pl_from_4d_data.py
+++ data_analysis/make_3dpsnr_wrti_pl_from_4d_data.py
@@ -10,11 +10,15 @@ def main(all_vols, pt_id):
     print(psnrs[547], ' db 3dpsnr')
     # Create and save the plot of PSNRs:
     fig,ax = plt.subplots(figsize=(12.5,6))
-    plt.plot(psnrs)
+    ax2 = ax.twinx()
+    ax.plot(psnrs)
+    ax2.plot(np.where(psnrs==np.inf)[0], np.ones(((psnrs==np.inf).sum())))
+    #(psnrs==np.inf)
     ax.set_title('3D PSNRs wrt the Initial Image')
     ax.set_xlabel('Time Index')
     ax.set_ylabel('PSNR (dB)')
     ax.set_xticks(np.arange(0,psnrs.shape[0],step=50))
+    
     plt.show()
     plt.savefig(f'/raid/yesiloglu/data/real_time_volumetric_mri/{pt_id}/temporal_evol_gifs/3dpsnrs_wrt_init', dpi=96, bbox_inches='tight')
     plt.close()

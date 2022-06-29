03ded73c85fb108f79d550623aef246da54e6ac6
RidvanYesiloglu <ridvan@stanford.edu>
2022-06-28 17:51:02 -0700

    Update make_gif_from_4d_data.py

--- data_analysis/make_gif_from_4d_data.py
+++ data_analysis/make_gif_from_4d_data.py
@@ -58,7 +58,7 @@ def main(all_vols, pt_id, ax_cr_sg, plot_most_fluc=False):
         ax.set_yticklabels(np.arange(0,psnrs.shape[0],step=50))
         fig.colorbar(im, ax=ax, orientation='vertical')
         plt.show()
-        plt.savefig(f'{gifs_dir}/{im_type_str}_psnrs_wrt_init', dpi=70)
+        plt.savefig(f'{gifs_dir}/{im_type_str}_psnrs_wrt_init', dpi=150, bbox_inches='tight')
         plt.close()
     
     # # Create gif

836a2d91f3d4eaf6adbaacefbac1e2aba88dc24e
RidvanYesiloglu <ridvan@stanford.edu>
2022-07-04 10:36:46 -0700

    Update make_gif_from_4d_data.py

--- data_analysis/make_gif_from_4d_data.py
+++ data_analysis/make_gif_from_4d_data.py
@@ -97,7 +97,7 @@ def main(all_vols, pt_id, ax_cr_sg, plot_most_fluc=False):
             plt.suptitle(f"{im_type_str.capitalize()} Images ({pt_id}, Time Point: {t:3d})")   
         plt.show()
         plt.tight_layout()
-        plt.savefig(filename, bbox_inches='tight')
+        plt.savefig(filename, bbox_inches='tight', dpi=350)
         plt.close()
 
     with imageio.get_writer(f'{gifs_dir}/{gif_name}.gif', mode='I') as writer:

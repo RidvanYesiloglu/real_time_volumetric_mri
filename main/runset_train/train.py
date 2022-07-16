d8a28e259d460a826b2f499f2dbd3fc9decd8a82
RidvanYesiloglu <ridvan@stanford.edu>
2022-07-16 15:17:46 -0700

    Update train.py

--- main/runset_train/train.py
+++ main/runset_train/train.py
@@ -71,7 +71,8 @@ def main(args=None, im_ind=None):
             train_loss = preruni_dict['main_module']()
             train_loss.backward()
             
-            rate = min(args.lr_im, ep*(args.lr_im-start_lr)/warmup_steps + start_lr)
+            #rate = min(args.lr_im, ep*(args.lr_im-start_lr)/warmup_steps + start_lr)
+            rate = min(args.lr_im,start_lr*((args.lr_im/start_lr)**(ep/warmup_steps)))
             for optimizer in preruni_dict['main_module'].optims:
                 for p in optimizer.param_groups:
                     p['lr'] = rate

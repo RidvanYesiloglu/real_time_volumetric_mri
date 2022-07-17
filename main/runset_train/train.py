d0964da3b19aa9a7084136a5b2c711ff45ac5294
RidvanYesiloglu <ridvan@stanford.edu>
2022-07-16 17:15:12 -0700

    Update train.py

--- main/runset_train/train.py
+++ main/runset_train/train.py
@@ -78,24 +78,19 @@ def main(args=None, im_ind=None):
             
             #'pri_emb','trn_wo_trns','trn_w_trns'
             # Save the model and the reconstruction
-            if (((ep+1)%50==0) or (test_psnr == max(psnrs_r))):
+            if ((args.conf == 'pri_emb') and (test_psnr == max(psnrs_r))):
                 #wr_acts.write_freq_actions({'args':args, 't':ep, 'start_time':start_time, 'res_dir': res_dir, 'run_number':run_number,\
                 #    'repr_str':repr_str,'psnrs_r':psnrs_r,'ssims_r':ssims_r,'losses_r':losses_r}, preruni_dict)
             
                 # Save the test output and the model:
                 output_im, test_psnr , test_ssim, test_loss = preruni_dict['main_module'].test_psnr_ssim(ret_im=True)
-                if ((ep+1)%50==0):
-                    model_name = os.path.join(res_dir, 'dd_md_{}_ep{}_{:.4g}dB.pt'.format(repr_str, ep+1, test_psnr))
-                    torch.save(preruni_dict['main_module'].get_to_save_dict(), model_name)
-                    np.save(os.path.join(res_dir,'dd_rec_{}_ep{}_{:.4g}dB'.format(repr_str, ep+1, test_psnr)), output_im.detach().cpu().numpy())
-                else:
-                    for filename in glob.glob(os.path.join(res_dir, 'md_{}*'.format(repr_str))):
-                        os.remove(filename)
-                    for filename in glob.glob(os.path.join(res_dir, 'rec_{}*'.format(repr_str))):
-                        os.remove(filename)
-                    model_name = os.path.join(res_dir, 'md_{}_ep{}_{:.4g}dB.pt'.format(repr_str, ep+1, test_psnr))
-                    torch.save(preruni_dict['main_module'].get_to_save_dict(), model_name)
-                    np.save(os.path.join(res_dir,'rec_{}_ep{}_{:.4g}dB'.format(repr_str, ep+1, test_psnr)), output_im.detach().cpu().numpy())
+                for filename in glob.glob(os.path.join(res_dir, 'md_{}*'.format(repr_str))):
+                    os.remove(filename)
+                for filename in glob.glob(os.path.join(res_dir, 'rec_{}*'.format(repr_str))):
+                    os.remove(filename)
+                model_name = os.path.join(res_dir, 'md_{}_ep{}_{:.4g}dB.pt'.format(repr_str, ep+1, test_psnr))
+                torch.save(preruni_dict['main_module'].get_to_save_dict(), model_name)
+                np.save(os.path.join(res_dir,'rec_{}_ep{}_{:.4g}dB'.format(repr_str, ep+1, test_psnr)), output_im.detach().cpu().numpy())
                 np.save(os.path.join(res_dir,'psnrs_r{}'.format(run_number)), np.asarray(psnrs_r))
                 np.save(os.path.join(res_dir,'ssims_r{}'.format(run_number)), np.asarray(ssims_r))
                 np.save(os.path.join(res_dir,'losses_r{}'.format(run_number)), np.asarray(losses_r))

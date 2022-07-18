8f172903bf86d34657ec3059c9a05a25578deabe
RidvanYesiloglu <ridvan@stanford.edu>
2022-07-18 01:24:57 -0700

    Update train.py

--- main/runset_train/train.py
+++ main/runset_train/train.py
@@ -37,9 +37,9 @@ def main(args=None):
     dtype = torch.float32
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     np.set_printoptions(precision=7)
-    print_freq = 100 # print results once in "print_frequency" epochs 
-    write_freq = 100 # print results once in "print_frequency" epochs
-    gif_freq = 100
+    print_freq = 1000 # print results once in "print_frequency" epochs 
+    write_freq = 1000 # print results once in "print_frequency" epochs
+    gif_freq = 1000
     cudnn.benchmark = True
     
     if not os.path.exists(res_dir):
@@ -70,7 +70,7 @@ def main(args=None):
             
             optimizer.step()
                 
-            test_psnr , test_ssim, test_loss = preruni_dict['main_module'].test_psnr_ssim()
+            output_im, test_psnr, test_ssim, test_loss = preruni_dict['main_module'].test_psnr_ssim(ret_im=True)
             losses_r.append(test_loss)
             psnrs_r.append(test_psnr)
             ssims_r.append(test_ssim)
@@ -82,7 +82,6 @@ def main(args=None):
                 #    'repr_str':repr_str,'psnrs_r':psnrs_r,'ssims_r':ssims_r,'losses_r':losses_r}, preruni_dict)
             
                 # Save the test output and the model:
-                output_im, test_psnr , test_ssim, test_loss = preruni_dict['main_module'].test_psnr_ssim(ret_im=True)
                 for filename in glob.glob(os.path.join(res_dir, 'md_{}*'.format(repr_str))):
                     os.remove(filename)
                 for filename in glob.glob(os.path.join(res_dir, 'rec_{}*'.format(repr_str))):
@@ -93,27 +92,25 @@ def main(args=None):
                 np.save(os.path.join(res_dir,'psnrs_r{}'.format(run_number)), np.asarray(psnrs_r))
                 np.save(os.path.join(res_dir,'ssims_r{}'.format(run_number)), np.asarray(ssims_r))
                 np.save(os.path.join(res_dir,'losses_r{}'.format(run_number)), np.asarray(losses_r))
-            elif ((args.conf != 'pri_emb') and ((test_psnr == max(psnrs_r)) or ((ep+1)%1000==0))):
+            elif ((args.conf != 'pri_emb') and ((test_loss == min(losses_r)) and ((ep+1) > 150))):
                 #wr_acts.write_freq_actions({'args':args, 't':ep, 'start_time':start_time, 'res_dir': res_dir, 'run_number':run_number,\
                 #    'repr_str':repr_str,'psnrs_r':psnrs_r,'ssims_r':ssims_r,'losses_r':losses_r}, preruni_dict)
             
                 # Save the test output and the model:
-                output_im, test_psnr , test_ssim, test_loss = preruni_dict['main_module'].test_psnr_ssim(ret_im=True)
-                if ((ep+1)%1000==0):
-                    model_name = os.path.join(res_dir, 'dd_md_{}_ep{}_{:.4g}dB.pt'.format(repr_str, ep+1, test_psnr))
-                    torch.save(preruni_dict['main_module'].get_to_save_dict(), model_name)
-                    np.save(os.path.join(res_dir,'dd_rec_{}_ep{}_{:.4g}dB'.format(repr_str, ep+1, test_psnr)), output_im.detach().cpu().numpy())
-                if (test_psnr == max(psnrs_r)):
-                    for filename in glob.glob(os.path.join(res_dir, 'md_{}*'.format(repr_str))):
-                        os.remove(filename)
-                    for filename in glob.glob(os.path.join(res_dir, 'rec_{}*'.format(repr_str))):
-                        os.remove(filename)
-                    model_name = os.path.join(res_dir, 'md_{}_ep{}_{:.4g}dB.pt'.format(repr_str, ep+1, test_psnr))
-                    torch.save(preruni_dict['main_module'].get_to_save_dict(), model_name)
-                    np.save(os.path.join(res_dir,'rec_{}_ep{}_{:.4g}dB'.format(repr_str, ep+1, test_psnr)), output_im.detach().cpu().numpy())
-                np.save(os.path.join(res_dir,'psnrs_r{}'.format(run_number)), np.asarray(psnrs_r))
-                np.save(os.path.join(res_dir,'ssims_r{}'.format(run_number)), np.asarray(ssims_r))
-                np.save(os.path.join(res_dir,'losses_r{}'.format(run_number)), np.asarray(losses_r)) 
+                wr_acts.gif_freq_actions({'args':args, 't':ep, 'res_dir': res_dir, 'repr_str':repr_str}, preruni_dict)
+                # if ((ep+1)%1000==0):
+                #     model_name = os.path.join(res_dir, 'dd_md_{}_ep{}_{:.4g}dB.pt'.format(repr_str, ep+1, test_psnr))
+                #     torch.save(preruni_dict['main_module'].get_to_save_dict(), model_name)
+                #     np.save(os.path.join(res_dir,'dd_rec_{}_ep{}_{:.4g}dB'.format(repr_str, ep+1, test_psnr)), output_im.detach().cpu().numpy())
+                #if (test_psnr == max(psnrs_r)):
+                for filename in glob.glob(os.path.join(res_dir, 'md_{}*'.format(repr_str))):
+                    os.remove(filename)
+                for filename in glob.glob(os.path.join(res_dir, 'rec_{}*'.format(repr_str))):
+                    os.remove(filename)
+                model_name = os.path.join(res_dir, 'md_{}_ep{}_{:.4g}dB.pt'.format(repr_str, ep+1, test_psnr))
+                torch.save(preruni_dict['main_module'].get_to_save_dict(), model_name)
+                np.save(os.path.join(res_dir,'rec_{}_ep{}_{:.4g}dB'.format(repr_str, ep+1, test_psnr)), output_im.detach().cpu().numpy())
+                
             # if (args.conf == 2) and (test_psnr == max(psnrs_r)): # network training
             #     # Save the test output and the model:
             #     for filename in glob.glob(os.path.join(save_folder, 'savedmodel_run{}*'.format(run_number))):
@@ -173,9 +170,12 @@ def main(args=None):
             #                 'enc': preruni_dict['encoder'].B, \
             #                 'opt': preruni_dict['optim'].state_dict(), \
             #                 }, model_name)
+        np.save(os.path.join(res_dir,'psnrs_r{}'.format(run_number)), np.asarray(psnrs_r))
+        np.save(os.path.join(res_dir,'ssims_r{}'.format(run_number)), np.asarray(ssims_r))
+        np.save(os.path.join(res_dir,'losses_r{}'.format(run_number)), np.asarray(losses_r)) 
         wr_acts.write_freq_actions({'args':args, 't':ep, 'start_time':start_time, 'res_dir': res_dir, 'run_number':run_number,\
                     'repr_str':repr_str,'psnrs_r':psnrs_r,'ssims_r':ssims_r,'losses_r':losses_r}, preruni_dict)
-        wr_acts.gif_freq_actions({'args':args, 't':ep, 'res_dir': res_dir, 'repr_str':repr_str}, preruni_dict)
+        #wr_acts.gif_freq_actions({'args':args, 't':ep, 'res_dir': res_dir, 'repr_str':repr_str}, preruni_dict)
         #wr_acts.postrun_i_actions({'args':args, 'run_number':run_number, 'save_folder':save_folder, 'losses_r':losses_r, 'psnrs_r':psnrs_r,'device':device, \
          #                         't':t},preallruns_dict, preruni_dict)
 

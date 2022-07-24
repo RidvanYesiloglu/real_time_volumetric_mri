dd8483c90546562538e0df4b014c7385c7d23e1e
RidvanYesiloglu <ridvan@stanford.edu>
2022-07-23 20:58:11 -0700

    Update train_for_set_of_reg_cos.py

--- main/runset_train/train_for_set_of_reg_cos.py
+++ main/runset_train/train_for_set_of_reg_cos.py
@@ -67,7 +67,7 @@ def main(args):
                         continue
                     print('**************************************************************')
                     curr_ind += 1
-                    if curr_ind < 19:
+                    if curr_ind < 19 or curr_ind > 20:
                         continue
                     print('Current run number: {}/{}'.format(curr_ind, tot_runs))
                     args.conf = 'trn_wo_trns' if wt==0 else 'trn_w_trns'

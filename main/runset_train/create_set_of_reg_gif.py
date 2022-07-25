ddf8c55681a03cf46f7b2486ccf92a7304b7bdc6
RidvanYesiloglu <ridvan@stanford.edu>
2022-07-25 00:36:00 -0700

    Update create_set_of_reg_gif.py

--- main/runset_train/create_set_of_reg_gif.py
+++ main/runset_train/create_set_of_reg_gif.py
@@ -148,9 +148,9 @@ def make_gif_frames(args, recs, refs, psnrs, ssims, sps, ts, ax_cr_sg, sl_no, gi
                 else:
                     ax[i,j].set_title(f'TCC: {ts[j]:.0e}, SCC: {sps[i]:.0e}')
                 if i == (nrows-1): #-0.29
-                    ax[i,j].text(64, 152, f'{ts[j]:.0e}', color='red', size=14, ha="center", transform=ax[i,j].transData)
+                    ax[i,j].text(64, 152, f'{ts[j]:.0e}', color='red', size=14, ha="center", va="center", transform=ax[i,j].transData)
                 if j == 0:#-0.22
-                    ax[i,j].text(-22,64, f'{sps[i]:.0e}', color='red', size=14, ha="center", transform=ax[i,j].transData)
+                    ax[i,j].text(-22,64, f'{sps[i]:.0e}', color='red', size=14, ha="center", va="center", transform=ax[i,j].transData)
                 ax[i,j].text(0.5,-0.1-0.01*(ax_cr_sg==0)+0.03*(ax_cr_sg!=0), '({:.1f} dB, {:.3f})'.format(ps, ss), color=ps_color, size=11, ha="center", transform=ax[i,j].transAxes)
                 # divider = make_axes_locatable(ax[i,j])
                 # cax = divider.append_axes('right', size='5%', pad=0.05)

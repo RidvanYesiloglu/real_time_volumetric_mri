8b81438d9c55165d7df20065c9998af501a4d17e
RidvanYesiloglu <ridvan@stanford.edu>
2022-07-25 03:02:13 -0700

    Update create_set_of_reg_gif.py

--- main/runset_train/create_set_of_reg_gif.py
+++ main/runset_train/create_set_of_reg_gif.py
@@ -183,11 +183,11 @@ def make_gif_frames(args, recs, refs, psnrs, ssims, sps, ts, ax_cr_sg, sl_no, gi
                     ax[i,j].text(-40,64, f'{sps[i]:.0e}', color='red', size=14, ha="center", va="center", transform=ax[i,j].transData)
                     if i == ((nrows-1)//2):
                         ax[i,j].text(-65,140, 'Spatial Continuity Reg. Coefficient', color='red', size=18, ha="center", va="center", rotation='vertical', transform=ax[i,j].transData)
-                ax[i,j].text(0.5,-0.1-0.01*(ax_cr_sg==0)+0.03*(ax_cr_sg!=0), '({:.1f} dB, {:.3f})'.format(ps, ss), color='w', size=11, ha="center", transform=ax[i,j].transAxes)
+                ax[i,j].text(0.5,-0.1-0.01*(ax_cr_sg==0)+0.03*(ax_cr_sg!=0), '({:.1f} dB, {:.3f})'.format(ps, ss), color='black', size=11, ha="center", transform=ax[i,j].transAxes)
                 # divider = make_axes_locatable(ax[i,j])
                 # cax = divider.append_axes('right', size='5%', pad=0.05)
                 # fig.colorbar(im, cax=cax, orientation='vertical')
-        plt.subplots_adjust(left=0.04, right=0.55, bottom=0.05, top=0.935, wspace=0.09, hspace=0.22)
+        plt.subplots_adjust(left=0.04, right=0.55, bottom=0.05, top=0.935, wspace=0.09, hspace=0.24)
         tcc_arrow = patches.ConnectionPatch(
             (0,145),
             (128,145),
